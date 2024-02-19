c44f607db1c9169b9366b244e28b57beb50d18c0
rahul <rdas53300@gmail.com>
2024-02-20 01:11:42 +0530

    4th day

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,56 +1,59 @@
 import React, { useState } from 'react'
 import './Nav.css'
 import logo from '../../assets/logo.png'
-import { IoReorderThree } from "react-icons/io5";
 import { Link } from 'react-router-dom';
+import { TiThMenu } from "react-icons/ti";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   const [visible , setVisible] = useState(false);
 
-  const handleClik = (vissibe) => {
+  const handleToggle = (vissibe) => {
     setVisible(!visible);
   }
 
   return (
-    <div  className='bg-[#4608ad] border-b-black border-b-[1px]'>
-        <nav className='navbar'>
+    <div className='bg-[#4608ad] border-b-black border-b-[1px]'>
+    <nav className='navbar'>
+      <Link to={"/"}>
         <div>
           <img src={logo} width={150} alt='' className='text-white'></img>
         </div>
-        <div className=' flex max-sm:hidden gap-3'>
-          <Link to={"/login"}>
-            <button class="button-85" role="button">Login</button>
-          </Link>
-          <Link to={"/signup"}>
-            <button class="button-85" role="button">Sign up</button>
-          </Link>
-        </div>
+      </Link>
+      <div className='flex max-sm:hidden gap-3'>
+        <Link to={"/login"}>
+          <button className="button-85" role="button">Login</button>
+        </Link>
+        <Link to={"/signup"}>
+          <button className="button-85" role="button">Sign up</button>
+        </Link>
+      </div>
 
-        <button onClick={() => handleClik(true)} className=' max-sm:flex hidden justify-center items-center '>
-          <IoReorderThree className='text-3xl'/>
-        </button>
+      <button onClick={handleToggle} className='max-sm:flex hidden justify-center items-center'>
+        <TiThMenu className='text-2xl text-richblack-100'/>
+      </button>
 
-        {
-          visible === true && (
-            <div className='absolute w-[40%] p-2 bg-richblack-5 h-screen z-10 top-0 right-0   visible'>
-              <button onClick={() =>handleClik(true)} className=' max-sm:flex hidden justify-center items-center text-white '>
-                <IoReorderThree className='text-3xl text-white'/>
+      {
+        visible && (
+          <div className='fixed inset-0 flex items-center justify-center  bg-[#000000c4] z-50'>
+            <div className='bg-[#fe3333] p-4 rounded-lg'>
+              <button onClick={handleToggle} className='absolute top-0 left-0 m-4 text-white'>
+                <RxCross2 className='text-2xl text-red-500'/>
               </button>
-              <div className='flex flex-col gap-5 mt-5 '>
+              <div className='flex flex-col gap-5 mt-5 min-h-36 min-w-40 justify-center '>
                 <Link to={"/login"}>
-                  <button className='py-1 px-2 border-solid border-[1px] border-richblack-900 rounded-md hover:bg-richblack-900 hover:text-richblack-5'>Login</button>
+                  <button className='py-2 px-2 w-full text-richblack-50 bg-black text-lg font-semibold border-solid border-[2px] border-[#000000cf] rounded-md hover:bg-richblack-900 hover:text-richblack-5'>Login</button>
                 </Link>
-                <Link>
-                  <button className='py-1 px-2 border-solid border-[1px] border-richblack-900 rounded-md'>Sign up</button>
+                <Link to={"/signup"}>
+                  <button className='py-2 px-2 w-full text-richblack-50 border-solid bg-black text-lg  border-[2px] border-[#080707cf]  rounded-md'>Sign up</button>
                 </Link>
               </div>
             </div>
-          )
-        }
-
-      </nav>
-      
-    </div>
+          </div>
+        )
+      }
+    </nav>
+  </div>
   )
 }
 
